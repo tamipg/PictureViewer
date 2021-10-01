d9a5d6683ede03d958889bb60b4463337cbad6e4
Tamara <tami.pg94@gmail.com>
2021-10-01 20:45:12 +0200

    commit

--- PictureViewer/Form1.Designer.vb
+++ PictureViewer/Form1.Designer.vb
@@ -22,17 +22,157 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.showButton = New System.Windows.Forms.Button()
+        Me.clearButton = New System.Windows.Forms.Button()
+        Me.backgroundButton = New System.Windows.Forms.Button()
+        Me.closeButton = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.TableLayoutPanel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.FlowLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.PictureBox1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.CheckBox1, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.FlowLayoutPanel1, 1, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(578, 318)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.TableLayoutPanel1.SetColumnSpan(Me.PictureBox1, 2)
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(572, 280)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(3, 289)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(60, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Stretch"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.showButton)
+        Me.FlowLayoutPanel1.Controls.Add(Me.clearButton)
+        Me.FlowLayoutPanel1.Controls.Add(Me.backgroundButton)
+        Me.FlowLayoutPanel1.Controls.Add(Me.closeButton)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(89, 289)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(486, 26)
+        Me.FlowLayoutPanel1.TabIndex = 2
+        '
+        'showButton
+        '
+        Me.showButton.AutoSize = True
+        Me.showButton.Font = New System.Drawing.Font("Javanese Text", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.showButton.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.showButton.Location = New System.Drawing.Point(363, 3)
+        Me.showButton.Name = "showButton"
+        Me.showButton.Size = New System.Drawing.Size(120, 35)
+        Me.showButton.TabIndex = 0
+        Me.showButton.Text = "Mostrar una imagen"
+        Me.showButton.UseVisualStyleBackColor = True
+        '
+        'clearButton
+        '
+        Me.clearButton.AutoSize = True
+        Me.clearButton.Font = New System.Drawing.Font("Javanese Text", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clearButton.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.clearButton.Location = New System.Drawing.Point(255, 3)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(102, 35)
+        Me.clearButton.TabIndex = 1
+        Me.clearButton.Text = "Borrar la imagen"
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'backgroundButton
+        '
+        Me.backgroundButton.AutoSize = True
+        Me.backgroundButton.Font = New System.Drawing.Font("Javanese Text", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.backgroundButton.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.backgroundButton.Location = New System.Drawing.Point(85, 3)
+        Me.backgroundButton.Name = "backgroundButton"
+        Me.backgroundButton.Size = New System.Drawing.Size(164, 35)
+        Me.backgroundButton.TabIndex = 2
+        Me.backgroundButton.Text = "Establecer el color de fondo"
+        Me.backgroundButton.UseVisualStyleBackColor = True
+        '
+        'closeButton
+        '
+        Me.closeButton.AutoSize = True
+        Me.closeButton.Font = New System.Drawing.Font("Javanese Text", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.closeButton.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.closeButton.Location = New System.Drawing.Point(4, 3)
+        Me.closeButton.Name = "closeButton"
+        Me.closeButton.Size = New System.Drawing.Size(75, 35)
+        Me.closeButton.TabIndex = 3
+        Me.closeButton.Text = "Cerrar"
+        Me.closeButton.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" &
+    "s (*.*)|*.*"
+        Me.OpenFileDialog1.Title = "Select a picture file"
         '
         'Form1
         '
+        Me.AcceptButton = Me.showButton
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(534, 311)
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.CancelButton = Me.closeButton
+        Me.ClientSize = New System.Drawing.Size(578, 318)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
         Me.Name = "Form1"
         Me.Text = "Visor de imágenes"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents showButton As Button
+    Friend WithEvents clearButton As Button
+    Friend WithEvents backgroundButton As Button
+    Friend WithEvents closeButton As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ColorDialog1 As ColorDialog
 End Class
